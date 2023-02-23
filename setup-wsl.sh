45b0b43bae60ce78cbadebb902f41a90c4d870f4
alex-naud <63290314+alex-naud@users.noreply.github.com>
2023-02-23 09:37:12 +0400

    Refactor installation for python, R and postgreSQL

--- setup-wsl.sh
+++ setup-wsl.sh
@@ -14,25 +14,28 @@ echo -e "\n-- Installing various app from debian package manager"
 sudo apt update
 sudo apt upgrade  -y
 
-sudo apt install r-base -y
-sudo apt install python3 -y
-sudo apt install python3-pip -y
 sudo apt install vim -y
 sudo apt install tmux -y
 sudo apt install tree -y
 sudo apt install ripgrep -y
 sudo apt install fzf -y
 
-# Install from pip
+# Install from tldr
 echo -e "\n-- Installing from pip"
 sudo apt install python3-pip -y
 pip install tldr 
-pip install ipython 
-pip install radian
 
 # Install gh
 echo -e "\n-- Installing gh"
 $DOTFILES/install/install-gh
+
+# Install R
+echo -e "\n-- Installing R"
+$DOTFILES/install/install-R
+
+# Install postgresql
+echo -e "\n-- Installing PostgreSQL"
+$DOTFILES/install/install-postgre
 
 # Source all setup
 echo -e "\n-- App configurations"
@@ -40,6 +43,7 @@ echo -e "\n-- App configurations"
 echo -e "\n-bash"; $DOTFILES/bash/setup
 echo -e "\n-vim"; $DOTFILES/vim/setup
 echo -e "\n-tmux"; $DOTFILES/tmux/setup
+echo -e "\n-python"; $DOTFILES/python/setup
 
 # Final message
 echo -e "\n-- Done executing setup scripts"
